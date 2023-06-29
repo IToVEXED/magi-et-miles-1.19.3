0c24c11ac825143eea48a9cc8c748ef17dfe3359
IToVEXED <itovexed@gmail.com>
2023-06-29 02:15:46 -0500

    Im really finna stop organizing this ish

--- src/main/java/net/vexed/magimiles/util/ModLootTableModifiers.java
+++ src/main/java/net/vexed/magimiles/util/ModLootTableModifiers.java
@@ -3,18 +3,25 @@ package net.vexed.magimiles.util;
 import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.KilledByPlayerLootCondition;
+import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.network.packet.s2c.play.SetCameraEntityS2CPacket;
 import net.minecraft.util.Identifier;
 import net.vexed.magimiles.item.ModItems;
 
 public class ModLootTableModifiers {
     private static final Identifier NETHERRACK_BLOCK_ID
             = new Identifier("minecraft", "blocks/netherrack");
+    private static final Identifier GOLD_ORE_ID
+            = new Identifier("minecraft", "blocks/gold_ore");
+    private static final Identifier WITHER_SKELETON_ID
+            = new Identifier("minecraft", "entities/wither_skeleton");
 
     public static  void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -26,6 +33,25 @@ public class ModLootTableModifiers {
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
+            if(GOLD_ORE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(.35f))
+                        .with(ItemEntry.builder(ModItems.FOOLS_GOLD))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if(WITHER_SKELETON_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.2f))
+                        .conditionally(KilledByPlayerLootCondition.builder())
+                        .with(ItemEntry.builder(ModItems.CURSED_ASHES))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
         });
     }
+
+
 }
